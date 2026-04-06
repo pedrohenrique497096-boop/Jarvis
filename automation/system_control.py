5a44a03eea4ca5e0a1373acdafb97ff398c87fbf
Xxx <pedrohenrique497096@gmail.com>
2026-04-06 08:44:59 -0300

    Create system_control.py

---
+++ automation/system_control.py
@@ -0,0 +1,14 @@
+import os
+import webbrowser
+
+def open_notepad():
+    os.system("start notepad")
+
+def open_chrome():
+    os.system("start chrome")
+
+def open_youtube():
+    webbrowser.open("https://youtube.com")
+
+def open_google():
+    webbrowser.open("https://google.com")
